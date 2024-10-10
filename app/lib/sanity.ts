4179060008beb24ef79478df863e6ec5268f1dfe
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-10-09 19:13:53 -0700

    init commit

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -6,3 +6,4 @@ export const client = createClient({
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
 });
+ 
