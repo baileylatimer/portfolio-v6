4cfece2c8c75599acc39b72d5eac26d55909e050
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-10-09 19:35:21 -0700

    Simplify server function for debugging

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,23 +1,36 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars, no-undef */
 import { createRequestHandler } from "@remix-run/netlify";
 import * as build from "@remix-run/dev/server-build";
 
-const handler = createRequestHandler({
+const remixHandler = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
 });
 
 export default async (event, context) => {
+  console.log('Incoming request:', event);
+  
+  // Return a simple response for testing
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ message: 'Hello from Remix on Netlify!' }),
+  };
+
+  // Uncomment the following block when the simple response works
+  /*
   try {
-    console.log('Incoming request:', event);
-    const response = await handler(event, context);
+    const response = await remixHandler(event, context);
     console.log('Response:', response);
     return response;
   } catch (error) {
     console.error('Error in server.js:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
+      body: JSON.stringify({ error: 'Internal Server Error', details: error.message }),
     };
   }
+  */
 };
